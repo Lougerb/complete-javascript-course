f071f08eaaabba2d350a6488ef9c9bdc60441d81
Louein Gerald Baling <lgbalingbiz@gmail.com>
2021-03-06 20:31:47 +0800

    JS Fundamental part 1 Operators

---
+++ 01-Fundamentals-Part-1/starter/JSFund.js
@@ -0,0 +1,24 @@
+//Basic Operators Assignment
+
+//1
+let myContryPop = 1000000;
+let myHalfContryPop = myContryPop/2;
+console.log(myHalfContryPop);
+
+//2
+myContryPop++;
+console.log(myContryPop);
+
+//3
+const finlandPop=6000000;
+console.log(finlandPop < myContryPop);
+
+//4
+const avePopCont = 33000000;
+console.log(avePopCont>myContryPop);
+
+//5
+
+let country="Malaysia", continent="Asia", countryPopulation=11000000, countryLanguage="Malay";
+let description = country + " is in " + continent + ", and its " + countryPopulation + " people speaks " + countryLanguage;
+console.log(description);
