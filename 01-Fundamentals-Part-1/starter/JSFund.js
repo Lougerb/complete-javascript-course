06357cc3d5c49354274f392438189320b922d7f0
Louein Gerald Baling <lgbalingbiz@gmail.com>
2021-03-07 23:09:54 +0800

    Doing my own code

--- 01-Fundamentals-Part-1/starter/JSFund.js
+++ 01-Fundamentals-Part-1/starter/JSFund.js
@@ -89,19 +89,98 @@ if(myCountryPop>33){
 else{
     console.log(`${theirCountry}'s population is ${33-myCountryPop} million below Average`);
 }
-*/
+
 
 //Assignment
 
-const markMass=78, markHeight=1.69;
-const johnMass=92, johnHeight=1.95;
+const markMass = 78, markHeight = 1.69;
+const johnMass = 92, johnHeight = 1.95;
 
 const markBMI = markMass / markHeight ** 2;
 const johnBMI = johnMass / johnHeight ** 2;
 
-if(markBMI>johnBMI){
+if (markBMI > johnBMI) {
     console.log(`Mark has the highest BMI of ${markBMI} than John's BMI of ${johnBMI}`);
 }
-else{
+else {
     console.log(`John has the highest BMI of ${johnBMI} than Mark's BMI of ${markBMI}`);
 }
+
+//A=wl
+
+const hisWrec = 16, hisHrec = 10;
+const herWrec = 10, herHrec = 19;
+
+const hisArec = hisWrec * hisHrec;
+const herArec = herWrec * herHrec;
+
+if (hisArec >= herArec) {
+    console.log(`His area ${hisArec} is larger than her area ${herArec}.`);
+}
+else {
+    console.log(`Her area ${herArec} is larger than his ${hisArec}`);
+}
+
+//else if
+
+const age = 18;
+if (age == 18) {
+    console.log("You are legal NOW!");
+}
+else if (18 > age) {
+    console.log("You are not legal yet")
+}
+else if (18 < age) {
+    console.log("You are already an Adult");
+}
+
+
+
+// what does === do?---------------------------------
+// this condition will only express if with the same type of value
+//for example String to String, Number to Number
+
+
+//string
+let myNameG = "gerald";
+
+if (myNameG === "gerald") {
+    console.log(`His name is ${myNameG}`);
+}
+else {
+    console.log(`His name is no ${myNameG}`);
+}
+
+//number
+let age = 19;
+
+if (age === 19) {
+    console.log(`He is ${age} years old`);
+}
+
+//if being compared with different Value, like String to number, it will give a False result
+
+let year = "2001"; //Value is string
+if (year === 2001) {  //compared to a Number
+    console.log(`Today is ${year}`); //this will only show if compared to Number to number OR String to string
+}
+//result will not appear if not compared correctly
+//else, make an else statement.
+else {
+    console.log(`Give me a number`);
+}
+*/
+//To fix this, try converting a string to number
+
+let date = "23";
+date = Number(date);
+
+if (date === 23) {
+    console.log(`Today's date is ${date}`);
+}
+//this else statement is optional
+else {
+    console.log(`Give me a number`);
+}
+
+
