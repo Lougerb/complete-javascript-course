318ae2faa2e3d2f3bf898da0c68f1c87299da6cb
Louein Gerald Baling <lgbalingbiz@gmail.com>
2021-03-20 00:40:06 +0800

    add note

--- MyProject/Todo List/script.js
+++ MyProject/Todo List/script.js
@@ -4,3 +4,14 @@ const getTitle = document.getElementById("createTitle").textContent;
 const getNote = document.getElementById("writeNote").textContent;
 const btnAddNote = document.getElementById("addNote");
 const noteStyle = document.querySelector("noteBG");
+const notesContainer = document.getElementById("noteContainer");
+
+const createNewNote = function () {
+  const createDiv = document.createElement("div");
+  createDiv.classList.add("noteBG");
+  createDiv.textContent = "asdsasd";
+  //Will create the new div inside the Note's container
+  notesContainer.appendChild(createDiv);
+};
+
+btnAddNote.addEventListener("click", createNewNote);
