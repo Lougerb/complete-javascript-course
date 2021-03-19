ed891f84d94ccb9f8342ffe76eb089ba69c111d4
Louein Gerald Baling <lgbalingbiz@gmail.com>
2021-03-19 20:26:57 +0800

    Note Project

---
+++ MyProject/Todo List/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const getTitle = document.getElementById("createTitle").textContent;
+const getNote = document.getElementById("writeNote").textContent;
+const btnAddNote = document.getElementById("addNote");
+const noteStyle = document.querySelector("noteBG");
