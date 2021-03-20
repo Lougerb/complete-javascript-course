67a57cb212d3300468311a20a0a66ac193a8a717
Louein Gerald Baling <lgbalingbiz@gmail.com>
2021-03-20 20:31:27 +0800

    generating done

--- MyProject/Todo List/script.js
+++ MyProject/Todo List/script.js
@@ -1,17 +1,55 @@
 "use strict";
 
-const getTitle = document.getElementById("createTitle").textContent;
-const getNote = document.getElementById("writeNote").textContent;
+let getTitle = document.querySelector(".createTitle");
+let getNote = document.querySelector("#writeNote");
 const btnAddNote = document.getElementById("addNote");
-const noteStyle = document.querySelector("noteBG");
+const noteStyle = document.querySelector(".noteBG");
 const notesContainer = document.getElementById("noteContainer");
 
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+
 const createNewNote = function () {
-  const createDiv = document.createElement("div");
+  //new div
+  const createDiv = document.createElement("form");
+  //for Option Button
+  const createOptionBtn = document.createElement("button");
+  const newInputTitle = document.createElement("input");
+  const newNoteContent = document.createElement("textarea");
+  //   const newTitle = document.getElementsByClassName("generateTitle");
+
+  //needs to change to icon!!!!!!!!!--------------------------
+  createOptionBtn.textContent = `...`;
+
+  //Will Generate Note's div
   createDiv.classList.add("noteBG");
-  createDiv.textContent = "asdsasd";
+
+  //Button
+  createOptionBtn.classList.add("optionBtn");
+
+  //Generate Title
+  newInputTitle.setAttribute("type", "text");
+  newInputTitle.classList.add("generateTitle");
+
+  //Generate new Notes Content
+  newNoteContent.classList.add("newNote");
+
   //Will create the new div inside the Note's container
+  //notesContainer=Parent
+  //createDiv=Child
   notesContainer.appendChild(createDiv);
+  notesContainer.appendChild(createOptionBtn);
+  createDiv.appendChild(newInputTitle);
+  createDiv.appendChild(newNoteContent);
+
+  //After creating components, it gets the value from Note creator of the created Note
+  document.querySelector(".generateTitle").value = getTitle.value;
+  document.querySelector(".newNote").value = getNote.value;
+
+  //clears the Note creator's content
+  getTitle.value = "";
+  getNote.value = "";
 };
 
 btnAddNote.addEventListener("click", createNewNote);
