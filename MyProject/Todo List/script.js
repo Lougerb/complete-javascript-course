a651e919e192671dc416533420480c59975aeac4
Louein Gerald Baling <lgbalingbiz@gmail.com>
2021-03-22 18:05:48 +0800

    Finally Working as intended

--- MyProject/Todo List/script.js
+++ MyProject/Todo List/script.js
@@ -53,16 +53,13 @@ const createNewNote = function () {
   div4OptBtn.classList.add("div4Btn");
   createDiv.classList.add("newNoteContent");
   notesContainer.appendChild(div4OptBtn);
-
   div4OptBtn.appendChild(createDiv);
-
   createDiv.appendChild(newInputTitle);
-
+  //Div for Option button to make Button appear above Note
   div4OptBtn.appendChild(div4drpDwnBtn);
   div4drpDwnBtn.classList.add("drpDownList");
-
+  //Drop Down menu
   div4drpDwnBtn.appendChild(createOptionBtn);
-
   div4drpDwnBtn.appendChild(drpDwnUl);
   drpDwnUl.appendChild(editDrpDwnLi);
   drpDwnUl.appendChild(deleteDrpDwnLi);
@@ -100,27 +97,79 @@ const createNewNote = function () {
   });
 
   //EDIT BUTTON=============================================================
+
+  ////////////////////////////////
   editDrpDwnLi.addEventListener("click", function () {
+    drpDwnUl.classList.add("hideOptBtn");
+
+    const _makeFormMiddle = document.querySelector(".makeFormMiddle");
+    const _div4SaveBtn = document.createElement("div");
+    const _editMyNote = document.createElement("form");
+    const _editMyTitle = document.createElement("input");
+    const _editNoteContent = document.createElement("textarea");
+    const _saveBtn = document.createElement("button");
+
+    _makeFormMiddle.appendChild(_div4SaveBtn);
+    _div4SaveBtn.appendChild(_editMyNote);
+    _editMyNote.appendChild(_editMyTitle);
+    _editMyNote.appendChild(_editNoteContent);
+    _div4SaveBtn.appendChild(_saveBtn);
+
+    _div4SaveBtn.classList.add("div4SaveBtn");
+    _editMyNote.classList.add("editMyNote");
+    _editMyNote.classList.add("noteBG");
+    _editMyNote.classList.add("newNoteContent");
+    _editMyTitle.classList.add("editMyTitle");
+    _editNoteContent.classList.add("editNoteContent");
+    _saveBtn.classList.add("saveBtn");
+
+    _editMyTitle.setAttribute("type", "text");
+    _editMyTitle.setAttribute("placeholder", "Empty Title...");
+    _editMyNote.setAttribute("placeholder", "Empty Note...");
+    _saveBtn.innerHTML = "Save";
+
     const _editOverlay = document.querySelector(".editOverlay");
-    const _editMyNote = document.querySelector(".editMyNote");
+    const _saveButton = document.querySelector(".saveBtn");
 
     //To show Overlay and Note Editor of the current Note
-    _editMyNote.classList.remove("editHidden");
     _editOverlay.classList.remove("editHidden");
 
     //Click anywhere on Overlay then closes Edit without Save change
     document.addEventListener("mouseup", function (e) {
       if (_editOverlay.contains(e.target)) {
+        _div4SaveBtn.remove();
         _editOverlay.classList.add("editHidden");
-        _editMyNote.classList.add("editHidden");
       }
     });
 
-    console.log("I am Working");
+    //Get values from Current Notes
+    _editMyTitle.value = newInputTitle.value;
+    _editNoteContent.value = newNoteContent.value;
+
+    //Save Button=======================================================
+    _saveButton.addEventListener("click", function () {
+      //Replace Current Note/Title content
+      newInputTitle.value = _editMyTitle.value;
+      newNoteContent.value = _editNoteContent.value;
+
+      _div4SaveBtn.remove();
+      //exits editor
+      _editOverlay.classList.add("editHidden");
+    });
+
+    //Edit Button Note:
+    //Instead of hiding/showing an editor
+    //I instead pick generating an editor per note
+    //in order to prevent saving edit's value to other notes (BUG)
+    //then when user close the editor, I use .remove()
+    //to remove the parent's or the whole editor's existence
   });
 
-  //Delete Button
-  // const deleteBtn = document.
+  //DELETE BUTTON=============================================================
+  deleteDrpDwnLi.addEventListener("click", function () {
+    //Will delete The parent div with the childrens
+    div4OptBtn.remove();
+  });
 };
 
 btnAddNote.addEventListener("click", createNewNote);
