b2d6f5a1f6ce479d2715bcdc0b62d0c040a9d629
Louein Gerald Baling <lgbalingbiz@gmail.com>
2021-04-14 19:53:12 +0800

    Dog challenge done

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -165,6 +165,81 @@ const currencies = new Map([
   ["GBP", "Pound sterling"],
 ]);
 
-currencies.forEach(function (fe_value, fe_key, fe_Map) {
-  console.log(`${fe_value}, ${fe_key}, ${fe_Map}`);
-});
+// currencies.forEach(function (fe_value, fe_key, fe_Map) {
+//   console.log(`${fe_value}, ${fe_key}, ${ fe_Map}`);
+// });
+
+//Coding Challenge
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. 
+So each of them asked 5 dog owners about their dog's age,
+and stored the data into an array (one array for each). 
+For now, they are just interested in knowing whether a dog is an adult or a puppy.
+A dog is an adult if it is at least 3 years old, and it's a puppy 
+if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages 
+('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually 
+have cats, not dogs! So create a shallow copy of Julia's array, 
+and remove the cat ages from that copied array 
+(because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult 
+("Dog number 1 is an adult, and is 5 years old") or a puppy 
+("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+/* 
+const julia_Vet = {
+    vetName: "Julia",
+    animalAge: [3, 5, 2, 12, 7],
+    animalAge2: [9, 16, 6, 8, 3],
+  },
+  kate_Vet = {
+    vetName: "Kate",
+    animalAge: [4, 1, 15, 8, 3],
+    animalAge2: [10, 5, 6, 1, 4],
+  };
+
+const checkanimalAge = function (vet) {
+  const getAllAge = [...vet.animalAge, ...vet.animalAge2];
+
+  let vetDogArr = (vet.dogAge = []);
+  let vetCatArr = (vet.catAge = []);
+
+  getAllAge.forEach(function (pAge) {
+    if (pAge < 13) {
+      vetDogArr.push(pAge);
+    } else {
+      vetCatArr.push(pAge);
+    }
+  });
+
+  vetDogArr.forEach(function (dAge, dIndex) {
+    const dog_Stage = function () {
+      const isOld = dAge >= 3 ? `an Adult` : `still a Puppy 🐶`;
+      return isOld;
+    };
+    console.log(
+      `Dog number ${dIndex + 1} is ${dog_Stage()}, and is ${dAge} years old`
+    );
+  });
+};
+
+checkanimalAge(julia_Vet);
+console.log(`-----`);
+checkanimalAge(kate_Vet);
+console.log(`-----`);
+*/
